8a1af597b05aa46e1449b5948994e35a26a05c0f
Lorenzo Pagani <larrypg2@gmail.com>
2024-01-29 10:16:05 +0100

    feat: adds edit and update

--- resources/views/comics/comics.blade.php
+++ resources/views/comics/comics.blade.php
@@ -2,22 +2,28 @@
 
 @section('content')
     <h1>comics list</h1>
-
-    @foreach ($comics as $comic)
-        <div class="card" style="width:18rem;">
-            <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{ $comic->title }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-                <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Guarda i dettagli</a>
-                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger" type="submit">cancella</button>
-                </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-wrap justify-content-between">
+                @foreach ($comics as $comic)
+                    <div class="card m-2" style="width:18rem;">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's
+                                content.</p>
+                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Guarda i dettagli</a>
+                            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">cancella</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-    @endforeach
+    </div>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,6 +6,7 @@
             <h5 class="card-title">{{ $comic->title }}</h5>
             <h6 class="card-subtitle mb-2 text-muted ">{{ $comic->series }}</h6>
             <p class="card-text">{{ $comic->description }}</p>
+            <a class="btn btn-primary" href="{{ route('comics.edit', $comic->id) }}">Edit</a>
         </div>
     </div>
 @endsection
